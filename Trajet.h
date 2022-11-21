abe071f63d85eec43e2cd0fb2e04e599ef047adf
XlRmlt <118745549+XlRmlt@users.noreply.github.com>
2022-11-21 15:42:05 +0100

    Add files via upload

---
+++ Trajet.h
@@ -0,0 +1,77 @@
+/*************************************************************************
+                           Xxx  -  description
+                             -------------------
+    début                : $DATE$
+    copyright            : (C) $YEAR$ par $AUTHOR$
+    e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Interface de la classe <Xxx> (fichier Xxx.h) ----------------
+#if ! defined ( Trajet_H )
+#define Trajet_H
+
+//--------------------------------------------------- Interfaces utilisées
+
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Rôle de la classe <Xxx>
+//
+//
+//------------------------------------------------------------------------
+
+class Trajet : public Ancetre
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+void Afficher();
+//----------------------------------------------------- Méthodes publiques
+    // type Méthode ( liste des paramètres );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+
+//------------------------------------------------- Surcharge d'opérateurs
+    Xxx & operator = ( const Xxx & unXxx );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+
+//-------------------------------------------- Constructeurs - destructeur
+    Trajet ( const Trajet & unTrajet );
+    // Mode d'emploi (constructeur de copie) :
+    //
+    // Contrat :
+    //
+
+    Trajet ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    virtual ~Trajet ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+//----------------------------------------------------- Attributs protégés
+
+};
+
+//-------------------------------- Autres définitions dépendantes de <Xxx>
+
+#endif // XXX_H
